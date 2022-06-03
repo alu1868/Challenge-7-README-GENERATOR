5a5dcda2959175e506b3497d0b9c40a8a28bc8f6
alu1868 <alu1868@gmail.com>
2022-06-03 16:50:15 -0500

    install inquirer + set up inquirer questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,126 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+const fs = require("fs")
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // Title of project
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project? (REQUIRED)",
+        // checks to see if any input was recieved
+        validate: title => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log("project title is REQUIRED")
+                return false;
+            }
+        }
+    },
+
+    // enter description of project
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide a description for your project (REQUIRED)",
+        // checks to see if any input was recieved
+        validate: description => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log("project description is REQUIRED")
+                return false
+            }
+        }
+    },
+
+    // instructions for installation
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide steps for installation of your project (optional)"
+    },
+
+    // instructions to use
+    {
+        type: "input",
+        name: "instructions",
+        message: "Please provide instructions for how to use your project (optional)"
+    },
+
+    // instructions for testing
+    {
+        type: "input",
+        name: "testing",
+        message: "Please provide instructions for how to test your project (optional"
+    },
+
+    // choose your license
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Please select the license for your project",
+        choices: ["none", "MIT", "GPL 2.0", "GPL 3.0", "Apache 2.0", "BSD 2.0", "ISC", "LGPL 3.0", "Mozilla 2.0"]
+    },
+
+    // contributors
+    {
+        type: "input",
+        name: "contributors",
+        message: "Please list the contributors to your project"
+    },
+
+    // enter email
+    {
+        type: "input",
+        name: "github",
+        message: "Please provide your forwarding email (REQUIRED)",
+        validate: email => {
+            if (emailInput) {
+                return true
+            } else {
+                console.log("Forwarding email is REQUIRED")
+                return false
+            }
+        }
+    },
+
+    // enter your github
+    {
+        type: "input",
+        name: "github",
+        message: "Please provide your GitHub username (REQUIRED)",
+        validate: github => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log("GitHub username is REQUIRED")
+                return false
+            }
+        }
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile("./README.md", generateMarkdown(data), function(err){
+        if (err) {
+            return console.err(err);
+        }
+        console.log("README has been generated")
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then ((data) => {
+        writeToFile(data);
+    })
+}
 
 // Function call to initialize app
 init();
--- README.md
+++ README.md
@@ -1,3 +1,53 @@
-# Professional README Generator Starter Code
+# Professional README Generator
 
 [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+## Table of Contents
+1. [User Story](#user-story)
+2. [Acceptance Criteria](#acceptance-criteria)
+3. [Description](#description)
+4. [Installation](#installation)
+5. [Image](#image) 
+6. [Questions](#questions)
+
+# User Story
+    As a developer
+    I WANT a README generator
+    SO THAT I can quickly create a professional README for a new project
+
+# Acceptance Criteria
+    GIVEN a command-line application that accepts user input
+
+    WHEN I am prompted for information about my application repository
+    THEN a high-quality, professional README.md is generated with the title of my project and sections entitled: 
+        [DESCRIPTION, TABLE OF CONTENTS, INSTALLATION, USAGE, LICENSE, CONTRIBUTING, TESTS, QUESTIONS]
+
+    WHEN I enter my project title
+    THEN this is displayed as the title of the README
+
+    WHEN I enter a description, installation instructions, usage infromation, contribution guidelines, and test instructions
+    THEN this information is added to the sectiosn of the README entitled DESCRIPTION, INSTALLATION, USAGE, CONTRIBUTING, and TEST
+
+    WHEN I choose a license for my application from a list of options 
+    THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the appllication is covered under
+
+    WHEN I enter my GitHub username
+    THEN this is added to the selection of the README entitled QUESTIONS, with a link to my GitHub profile
+
+    WHEN I enter my email address
+    THEN this is added to the section of the README entitled QUESTIONS, with instructions on how to reach me with additioanl questions
+
+    WHEN I click on the links in the Table of Contents
+    THEN I am taken to the corresponding section of the README
+
+# Description
+
+# Installation
+
+# Image
+
+# Questions
+If you have any questions or concerns, feel free to contact me 
+* GitHub: <a href="https://github.com/alu1868">
+* Email: <a href="mailto:alu1868@gmail.com">
+
